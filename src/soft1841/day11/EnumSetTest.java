ca18d87a6c5f314a71a03e7cd8ec847b6d17d4ab
曾月1988818 <2414691160@qq.com>
2019-03-21 07:20:26 +0800

    中英文对照

---
+++ src/soft1841/day11/EnumSetTest.java
@@ -0,0 +1,89 @@
+package soft1841.day11;
+/**
+ * 中英文对照
+ * @zengyue
+ * 10.03.21
+ */
+
+import java.util.Scanner;
+
+enum Week {
+    MONDAY("----星期一"), TUESDAY("----星期二"), WEDNESDAY("----星期三"), THURSDAY("----星期四"), FRIDAY("----星期五"), SATURDAY("----星期六"), SUNDAY("----星期天");
+    String week;
+
+    Week(String week) {
+        this.week = week;
+
+    }
+
+    public String getName() {
+        return week;
+    }
+
+    public static Week getWeek(String week) {
+        switch (week) {
+            case "mon":
+                return Week.MONDAY;
+            case "tues":
+                return Week.TUESDAY;
+            case "wed":
+                return Week.WEDNESDAY;
+            case "thru":
+                return Week.THURSDAY;
+            case "fri":
+                return Week.FRIDAY;
+            case "sat":
+                return Week.SATURDAY;
+            case "sun":
+                return Week.SUNDAY;
+            default:
+                return Week.MONDAY;
+        }
+    }
+}
+
+
+public class EnumSetTest
+
+{
+    public static void main(String[] args) {
+        System.out.println("————星期的中英文对照————");
+        Week weekArray[] = Week.values();
+        for (Week k : weekArray) {
+            System.out.println(k + "    " + k.getName());
+        }
+
+        System.out.print("\n");
+        Scanner sc = new Scanner(System.in);
+        Week week = Week.getWeek(sc.next());
+        switch (week) {
+            case MONDAY:
+                System.out.println("MONDAY" + week.getName());
+                break;
+            case TUESDAY:
+                System.out.println("TUESDAY" + week.getName());
+                break;
+            case WEDNESDAY:
+                System.out.println("WEDNESDAY" + week.getName());
+                break;
+            case THURSDAY:
+                System.out.println("THURSDAY" + week.getName());
+                break;
+            case FRIDAY:
+                System.out.println("FRIDAY" + week.getName());
+                break;
+            case SATURDAY:
+                System.out.println("SATURDAY" + week.getName());
+                break;
+            case SUNDAY:
+                System.out.println("SUNDAY" + week.getName());
+                break;
+            default:
+                System.out.println();
+                break;
+        }
+        sc.close();
+    }
+}
+
+
