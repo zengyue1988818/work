3b37c6bf82bd89bc68edbc8ce4e0b8e674d7fc20
曾月1988818 <2414691160@qq.com>
2019-03-27 07:15:31 +0800

    gridlayout布局练习

--- src/soft1841/ElevenUnit/ReaderAndWriter.java
+++ src/soft1841/ElevenUnit/ReaderAndWriter.java
@@ -1,8 +1,10 @@
 package soft1841.ElevenUnit;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -21,7 +23,7 @@ public class ReaderAndWriter {
                     //如果文件不存在时，创建新的文件
                     file.createNewFile();
                 }
-                System.out.println("请输入要执行的操作号：（1.写入文件；2.读取文件");
+                System.out.println("请输入要执行的操作号：（1.写入文件；2.读取文件）");
                 //控制台输入
                 Scanner scanner = new Scanner(System.in);
                 //获得“要执行的操作序号”
@@ -48,8 +50,42 @@ public class ReaderAndWriter {
                         }
                         System.out.println("上述内容已写入到文本文件中！");
                         break;
+                        //控制台输入2
+                    case 2:
+                        //声明字符输入流
+                        FileReader fr = null;
+                        //"word.txt"中的字符数为0时，控制台输出“文本中的字符数为0！！！"
+                        if (file.length() ==0){
+                            System.out.println("文本中的字符数为0！！！");
+                        }else {
+                            //word.txt中的字符数不为零时
+                            try {
+                                //创建用来读取word.txt中的字符输入流
+                                fr = new FileReader(file);
+                                //创建可容纳1024个字符的数组，用来储存读取的字符数的缓冲区
+                                char[] cbuf = new char[1024];
+                                //初始化已读取的字符数
+                                int hasread = -1;
+                                //循环读取word.txt中的数据
+                                while ((hasread = fr.read(cbuf)) != -1){
+                                    //把char数组中的内容转换为String类型输出
+                                    System.out.println("文件word.txt中的内容： \n" +new String(cbuf,0,hasread));
+                                }
+                            }catch (IOException e){
+                                e.printStackTrace();
+                            }finally {
+                                //关闭字符输入流
+                                fr.close();
+                            }
+                        }
+                        break;
+                        default:
+                            System.out.println("请输入符合要求的有效数字！");
+                            break;
                 }
-            } catch (IOException e) {
+            } catch (InputMismatchException imexc) {
+                System.out.println("输入的文本格式不正确！请重新输入...");
+            }catch (IOException e){
                 e.printStackTrace();
             }
         }
---
+++ src/soft1841/day15/GridLayoutTest.java
@@ -0,0 +1,124 @@
+package soft1841.day15;
+/**
+ * GridLayout网格布局示例
+ * @zengyue
+ * 1.03.27
+ */
+
+import com.sun.org.apache.xml.internal.security.Init;
+import javafx.scene.control.ComboBox;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GridLayoutTest extends JFrame {
+    //定义一个面板数组
+    private JPanel[] panels;
+    private GridLayoutTest(){
+     init();
+     setTitle("GridLayout网格布局管理器");
+     //界面启动最大化
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+    public void init(){
+        //将整个容器背景颜色设置为白色
+        getContentPane().setBackground(new Color(255,255,255));
+        //定义三行三列九宫格，放置9个面板
+        GridLayout gridLayout = new GridLayout(3,3,50,30);
+        setLayout(gridLayout);
+        //创建面板数组
+        panels = new JPanel[9];
+        //循环创建每个面板对象
+        for (int i = 0; i <9;i++){
+            panels[i] = new JPanel();
+            //面板上加上边框
+            panels[i].setBorder(BorderFactory.createTitledBorder("面板" + (i +1)));
+            //面板依次加上网络布局管理器，变成九宫格
+            add(panels[i]);
+        }
+        //设置第一个面板背景色，其布局方式默认为flowlayout居中对齐
+        panels[0].setBackground(new Color(178,223,219));
+        //在第一个面板布局中加入5个按钮
+        for (int i = 0; i<5;i++){
+            panels[0].add(new JButton("科技"));
+        }
+        //设置第二个面板为Borderlayout边界布局，设置背景色
+        panels[1].setLayout(new BorderLayout(20,20));
+        panels[1].setBackground(new Color(0,172,193));
+        //5个方向加入按钮
+        panels[1].add(new JButton("北"),BorderLayout.NORTH);
+        panels[1].add(new JButton("南"),BorderLayout.SOUTH);
+        panels[1].add(new JButton("东"),BorderLayout.EAST);
+        panels[1].add(new JButton("西"),BorderLayout.WEST);
+        panels[1].add(new JButton("中"),BorderLayout.CENTER);
+        //设置第三个面板为Gridlayout网格布局，设置背景色
+        panels[2].setLayout(new GridLayout(3,3,10,10));
+        panels[2].setBackground(new Color(77,182,172));
+        //依次加入9个按钮
+        for(int i = 0;i<9;i++){
+            panels[2].add(new JButton(String.valueOf(i + 1)));
+        }
+        //设置第四个面板为BordLayout边界布局，设置背景色
+        panels[3].setLayout(new BorderLayout(30,30));
+        panels[3].setBackground(new Color(140, 153, 250));
+        //五个方向加入文本
+        panels[3].add(new JLabel("                " +
+                "北京"),BorderLayout.NORTH);
+        panels[3].add(new JLabel("               深圳"),BorderLayout.SOUTH);
+        panels[3].add(new JLabel("上海            "),BorderLayout.EAST);
+        panels[3].add(new JLabel("南京"),BorderLayout.WEST);
+        panels[3].add(new JLabel("        " +
+                "武汉"),BorderLayout.CENTER);
+        //设置第五个面板背景色，其布局方式默认为flowlayout居中对齐
+        panels[4].setLayout(new FlowLayout(30));
+        panels[4].setBackground(new Color(223, 157, 160));
+        //加入文本框
+       panels[4].add(new JTextField("语文"));
+        panels[4].add(new JTextField("数学"));
+        panels[4].add(new JTextField("英语"));
+        panels[4].add(new JTextField("计算机"));
+        panels[4].add(new JTextField("科学"));
+        panels[4].add(new JTextField("社会"));
+        //设置第六个面板背景色，其布局方式默认为Gridlayout网格布局
+        panels[5].setLayout(new GridLayout(3,3,10,10));
+        panels[5].setBackground(new Color(182, 132, 181));
+        //依次加入6个密码框
+        for (int i = 0; i<6;i++){
+            panels[5].add(new JPasswordField(String.valueOf(i + 1)));
+            //设置第七个面板为BordLayout边界布局，设置背景色
+            panels[6].setLayout(new BorderLayout(0,0));
+            panels[6].setBackground(new Color(255, 243, 181));
+            //加入一个文本框，一个按钮
+            panels[6].add(new JTextField("英国"),BorderLayout.NORTH);
+            panels[6].add(new TextField("美国"),BorderLayout.SOUTH);
+            panels[6].add(new JTextField("法国"),BorderLayout.EAST);
+            panels[6].add(new JTextField("新西兰"),BorderLayout.WEST);
+            panels[6].add(new JTextField("中国"),BorderLayout.CENTER);
+            //设置第八个面板背景色，其布局方式默认为Gridlayout网格布局
+            panels[7].setLayout(new GridLayout(3,3,10,10));
+            panels[7].setBackground(new Color(182, 139, 155));
+          for (int k = 0; k <4;k++){
+              panels[7].add(new JRadioButton(String.valueOf(k + 1)));
+              //设置第一个面板背景色，其布局方式默认为flowlayout居中对齐
+              panels[8].setBackground(new Color(218, 190, 223));
+              for (int s = 0; s<4; s++){
+                  panels[8].add(new JComboBox());
+              }
+          }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        //用系统样式替代Swing默认方式，会稍微好看一点
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        new GridLayoutTest();
+    }
+
+}
