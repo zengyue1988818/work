2d15aff650ab99f4a26296613647ac2db5d38dc7
曾月1988818 <2414691160@qq.com>
2019-04-17 05:48:38 +0800

    线程加入

---
+++ src/soft1841/day26/JoinDemo.java
@@ -0,0 +1,33 @@
+package soft1841.day26;
+
+public class JoinDemo extends Thread {
+    @Override
+    public void run() {
+        for (int i = 10;i>0;i--){
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("开始倒数");
+        try {
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        JoinDemo joinDemo = new JoinDemo();
+        Thread thread = new Thread(joinDemo);
+        thread.start();
+        try {
+            thread.join();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println("节目开始");
+    }
+}
